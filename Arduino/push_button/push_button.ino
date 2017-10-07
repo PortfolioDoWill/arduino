73a0ba54caf3aa3a52629bea3efb790c97531536
Rafael Willian <rafael.willian.engcomp@gmail.com>
2017-10-06 23:01:20 -0300

    Primeiro commit - atualizações e comentários nos códigos

--- Arduino/push_button/push_button.ino
+++ Arduino/push_button/push_button.ino
@@ -1,16 +1,17 @@
+//Setando os pinos que serão usados
 const int botao = 2;
 const int led = 3;
 
 void setup() 
 {
-  pinMode(botao, INPUT);
-  pinMode(led, OUTPUT);
+  pinMode(botao, INPUT); //Configurando o pino 2 como pino de entrada
+  pinMode(led, OUTPUT); //Configurando o pino 3 como pino de saída
 }
 
 void loop() 
 {
-  if(digitalRead(botao) == HIGH)
-    digitalWrite(led, HIGH);
+  if(digitalRead(botao) == HIGH) //No caso de o push button estar em nível alto
+    digitalWrite(led, HIGH); //O led acende
   else
-    digitalWrite(led, LOW);
+    digitalWrite(led, LOW); //Caso contrário, ele apaga
 }
--- Arduino/semaforo/semaforo.ino
+++ Arduino/semaforo/semaforo.ino
@@ -1,19 +1,32 @@
+//Configurando as portas
+const int vermelho = 2;
+const int amarelo = 3;
+const int verde = 4;
+//Configurando o tempo de delay
+const int tempo1 = 5000;
+const int tempo2 = 2000;
+
+//Método que é acionado para configuração, executa apenas uma vez por execução
 void setup() 
 {
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
+  //Setando as portas anteriormente criadas como saída
+  pinMode(vermelho, OUTPUT);
+  pinMode(amarelo, OUTPUT);
+  pinMode(verde, OUTPUT);
 }
 
+//Método que ascende o led por um tempo constante determinado
+void acende(int farol, int tempo)
+{
+    digitalWrite(farol, HIGH); //Coloca o pino passado por parâmetro em alta
+    delay(tempo); //Espera até que o tempo determinado seja finalizado
+    digitalWrite(farol, LOW); //Desliga o pino
+}
+
+//Função que se repete durante todo o procedimento... É o main loop
 void loop() 
 {
-  digitalWrite(2, HIGH);
-  delay(2000);
-  digitalWrite(2, LOW);
-  digitalWrite(3, HIGH);
-  delay(1000);
-  digitalWrite(3, LOW);
-  digitalWrite(4, HIGH);  
-  delay(2000);
-  digitalWrite(4, LOW);
+  acende(vermelho, tempo1);
+  acende(verde, tempo1);
+  acende(amarelo, tempo2);
 }
